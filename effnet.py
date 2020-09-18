7fefaf9c83e81b4e01083226ac2bb8181eba1424
Nahian Siddique <nsid10@hotmail.com>
2020-09-17 22:17:04 -0500

    Update

--- effnet.py
+++ effnet.py
@@ -6,16 +6,8 @@ from tensorflow.keras.layers import concatenate, Conv2D, Conv2DTranspose, Dropou
 
 from blocks import residual_block
 
-np.random.seed = 42
 
-print("Initializing")
-
-
-def efficientUnet():
-
-    input_shape = (512, 512, 1)  # ------------------------<
-    neurons = 8
-    d = 0.1  # dropout
+def efficientUnet(input_shape, neurons=8, droprate=0.1):
 
     encoder = EfficientNetB4(include_top=False, weights="imagenet", input_shape=input_shape)
     inputs = encoder.input
@@ -30,7 +22,7 @@ def efficientUnet():
     c1 = encoder.layers[342].output
     c1 = LeakyReLU(alpha=0.1)(c1)
     p1 = MaxPooling2D((2, 2))(c1)
-    p1 = Dropout(d)(p1)
+    p1 = Dropout(droprate)(p1)
 
     c0 = residual_block(p1, neurons * 32)
     c0 = residual_block(c0, neurons * 32)
@@ -39,22 +31,22 @@ def efficientUnet():
 
     u1 = Conv2DTranspose(neurons * 16, (2, 2), strides=(2, 2), padding="same")(c0)
     u1 = concatenate([u1, c1])
-    u1 = Dropout(d)(u1)
+    u1 = Dropout(droprate)(u1)
     u1 = residual_block(u1, neurons * 16)
 
     u2 = Conv2DTranspose(neurons * 8, (2, 2), strides=(2, 2), padding="same")(u1)
     u2 = concatenate([u2, c2])
-    u2 = Dropout(d)(u2)
+    u2 = Dropout(droprate)(u2)
     u2 = residual_block(u2, neurons * 8)
 
     u3 = Conv2DTranspose(neurons * 4, (2, 2), strides=(2, 2), padding="same")(u2)
     u3 = concatenate([u3, c3])
-    u3 = Dropout(d)(u3)
+    u3 = Dropout(droprate)(u3)
     u3 = residual_block(u3, neurons * 4)
 
     u4 = Conv2DTranspose(neurons * 2, (2, 2), strides=(2, 2), padding="same")(u3)
     u4 = concatenate([u4, c4])
-    u4 = Dropout(d)(u4)
+    u4 = Dropout(droprate)(u4)
     u4 = residual_block(u4, neurons * 2)
 
     out = Conv2D(1, (1, 1), activation="sigmoid")(u4)
--- main.py
+++ main.py
@@ -6,41 +6,48 @@ import tensorflow as tf
 
 from effnet import *
 
+np.random.seed = 42
 
-hgt, wdt, chn = 512, 512, 1
+h, w, c = 584, 565, 3
 
 
 # -------------------------------------------------------------------------------------------------
 
-training_images = "/home/nsid/Documents/Projects/Data/cells_isbi2012/train/images/"
-training_labels = "/home/nsid/Documents/Projects/Data/cells_isbi2012/train/labels/"
-testing_images = "/home/nsid/Documents/Projects/Data/cells_isbi2012/test/images/"
+# training_images = "/home/nsid/Documents/Projects/Data/DRIVE/training/images/"
+# training_labels = "/home/nsid/Documents/Projects/Data/DRIVE/training/1st_manual/"
+# training_masks = "/home/nsid/Documents/Projects/Data/DRIVE/training/mask/"
+# testing_images = "/home/nsid/Documents/Projects/Data/DRIVE/test/images/"
+# testing_masks = "/home/nsid/Documents/Projects/Data/DRIVE/test/mask/"
 
-train_img = next(os.walk(training_images))[2]
-train_lbs = next(os.walk(training_labels))[2]
-test_img = next(os.walk(testing_images))[2]
+# train_img = next(os.walk(training_images))[2]
+# train_lbs = next(os.walk(training_labels))[2]
+# train_msk = next(os.walk(training_masks))[2]
+# test_img = next(os.walk(testing_images))[2]
+# test_msk = next(os.walk(testing_masks))[2]
 
-X_train = np.zeros((len(train_img), hgt, wdt, chn), dtype=np.float32)
-Y_train = np.zeros((len(train_lbs), hgt, wdt, 1), dtype=np.bool)
-X_test = np.zeros((len(test_img), hgt, wdt, chn), dtype=np.float32)
+# X_train = np.zeros((len(train_img), h, w, c), dtype=np.float32)
+# Y_train = np.zeros((len(train_lbs), h, w, 1), dtype=np.bool)
+# X_test = np.zeros((len(test_img), h, w, c), dtype=np.float32)
 
-for n, file_id in enumerate(train_img):
-    img = io.imread(training_images + file_id)
-    img = np.reshape(img, (hgt, wdt, chn)) / 255
-    X_train[n] = img
+# print(train_img)
 
-    mask = io.imread(training_labels + file_id)
-    mask = np.reshape(mask, (hgt, wdt, chn))
-    Y_train[n] = mask
+# for n, file_id in enumerate(train_img):
+#     img = io.imread(training_images + file_id)
+#     img = np.reshape(img, (h, w, c)) / 255
+#     X_train[n] = img
 
-for n, file_id in enumerate(test_img):
-    img = io.imread(testing_images + file_id)
-    img = np.reshape(img, (hgt, wdt, chn)) / 255
-    X_test[n] = img
+#     mask = io.imread(training_labels + file_id)
+#     mask = np.reshape(mask, (h, w, c))
+#     Y_train[n] = mask
+
+# for n, file_id in enumerate(test_img):
+#     img = io.imread(testing_images + file_id)
+#     img = np.reshape(img, (h, w, c)) / 255
+#     X_test[n] = img
 
 # -------------------------------------------------------------------------------------------------
 
 
-box = efficientUnet()
+box = efficientUnet(input_shape=(h, w, c))
 box.summary()
 print("Completed")
