aa00dc92dc5426ef23aefc661ef27247055975ef
nsid <nahian10@outlook.com>
2020-12-01 19:21:44 -0600

    Changes made

--- main.py
+++ main.py
@@ -12,24 +12,25 @@ h, w, c = 584, 565, 3
 
 
 # -------------------------------------------------------------------------------------------------
+main_path = "/home/nsid/Documents/Projects/Data/DRIVE"
 
-# training_images = "/home/nsid/Documents/Projects/Data/DRIVE/training/images/"
-# training_labels = "/home/nsid/Documents/Projects/Data/DRIVE/training/1st_manual/"
-# training_masks = "/home/nsid/Documents/Projects/Data/DRIVE/training/mask/"
-# testing_images = "/home/nsid/Documents/Projects/Data/DRIVE/test/images/"
-# testing_masks = "/home/nsid/Documents/Projects/Data/DRIVE/test/mask/"
+training_images = f"{main_path}/training/images/"
+training_labels = f"{main_path}/training/1st_manual/"
+training_masks = f"{main_path}/training/mask/"
+testing_images = f"{main_path}/test/images/"
+testing_masks = f"{main_path}/test/mask/"
 
-# train_img = next(os.walk(training_images))[2]
-# train_lbs = next(os.walk(training_labels))[2]
-# train_msk = next(os.walk(training_masks))[2]
-# test_img = next(os.walk(testing_images))[2]
-# test_msk = next(os.walk(testing_masks))[2]
+train_img = next(os.walk(training_images))[2]
+train_lbs = next(os.walk(training_labels))[2]
+train_msk = next(os.walk(training_masks))[2]
+test_img = next(os.walk(testing_images))[2]
+test_msk = next(os.walk(testing_masks))[2]
 
 # X_train = np.zeros((len(train_img), h, w, c), dtype=np.float32)
 # Y_train = np.zeros((len(train_lbs), h, w, 1), dtype=np.bool)
 # X_test = np.zeros((len(test_img), h, w, c), dtype=np.float32)
 
-# print(train_img)
+print(train_img)
 
 # for n, file_id in enumerate(train_img):
 #     img = io.imread(training_images + file_id)
