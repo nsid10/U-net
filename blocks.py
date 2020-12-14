e9c467674876889c4f390f58371bbd4a0e58e2bf
nsid <nahian10@outlook.com>
2020-12-13 19:34:35 -0500

    Add BatchNorm

--- blocks.py
+++ blocks.py
@@ -2,11 +2,11 @@ from tensorflow.keras.layers import Add, BatchNormalization, Conv2D, LeakyReLU
 
 
 def residual_block(x, filters):
-    y = Conv2D(filters, (3, 3), strides=(1, 1), padding="same", activation=None)(x)
+    y = Conv2D(filters, (3, 3), strides=(1, 1), padding="same")(x)
     y = LeakyReLU(alpha=0.1)(y)
     y = BatchNormalization()(y)
 
-    y = Conv2D(filters, (3, 3), strides=(1, 1), padding="same", activation=None)(y)
+    y = Conv2D(filters, (3, 3), strides=(1, 1), padding="same")(y)
     y = LeakyReLU(alpha=0.1)(y)
     y = BatchNormalization()(y)
 
