f54368f145d70974e040dc585bd23c787122f44b
nsid <nahian10@outlook.com>
2021-09-12 17:40:33 -0400

    added jaccard index to metrics

--- metrics.py
+++ metrics.py
@@ -1,24 +1,5 @@
-import os
-
 import numpy as np
-from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, roc_auc_score
-
-
-main_path = ""
-
-
-# loading test data
-testing_labels = f"{main_path}/labels/test/"
-test_lbl = next(os.walk(testing_labels))[2]
-test_lbl.sort()
-y_true = np.concatenate([np.load(testing_labels + file_id)["arr_0"] for file_id in test_lbl], axis=0)
-y_true = y_true.astype("float64")  # / 255
-y_true = y_true.flatten()
-
-
-# loading predictions
-y_pred = np.load(f"{main_path}/predictions/preds.npz")["arr_0"]
-y_pred = y_pred.flatten()
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, jaccard_score, roc_auc_score
 
 
 def dice_coef(y_true: np.ndarray, y_pred: np.ndarray) -> float:
@@ -30,18 +11,19 @@ def dice_coef(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     return (2.0 * np.sum(y_true_f * y_pred_f)) / (np.sum(y_true_f) + np.sum(y_pred_f))
 
 
-def image_metrics(y_true: np.ndarray, y_pred: np.ndarray, lim=0.5) -> tuple(float):
+def image_metrics(y_true: np.ndarray, y_pred: np.ndarray, lim=0.5):
     """
-    Calculates Area Under the Curve, F-score, Accuracy, Sensitivity, and Specificity
+    Calculates Area Under the Curve, F-score, Accuracy, Sensitivity, Specificity, Jaccard Index
     """
-    Y_pred_bin = np.zeros_like(y_true)
-    idx = y_pred > lim
-    Y_pred_bin[idx] = 1
+    y_pred_bin = np.zeros_like(y_true)
+    idx = y_pred >= lim
+    y_pred_bin[idx] = 1
 
     auc = roc_auc_score(y_true, y_pred)
-    f1 = f1_score(y_true, Y_pred_bin)
-    acc = accuracy_score(y_true, Y_pred_bin)
-    mat = confusion_matrix(y_true, Y_pred_bin)
+    f1 = f1_score(y_true, y_pred_bin)
+    acc = accuracy_score(y_true, y_pred_bin)
+    mat = confusion_matrix(y_true, y_pred_bin)
+    jac = jaccard_score(y_true, y_pred_bin, normalize=True)
 
     TN = mat[0][0]
     FN = mat[1][0]
@@ -51,11 +33,4 @@ def image_metrics(y_true: np.ndarray, y_pred: np.ndarray, lim=0.5) -> tuple(floa
     sen = TP / (TP + FN)
     spe = TN / (TN + FP)
 
-    return auc, f1, acc, sen, spe
-
-
-dice = dice_coef(y_true, y_pred)
-auc, f1, acc, sen, spe = image_metrics(y_true, y_pred, lim=0.5)
-
-
-print(f"Accuracy = \t{acc}\nf1score\t = \t{f1}\nAUC\t = \t{auc}\nDice\t = \t{dice}\nSensitivity = \t{sen}\nSpecificity = \t{spe}")
+    return auc, f1, acc, sen, spe, jac
--- test_model.py
+++ test_model.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import tensorflow as tf
 
+from metrics import dice_coef, image_metrics
 
 main_path = ""
 
@@ -14,9 +15,23 @@ test_img.sort()
 x_test = np.concatenate([np.load(testing_images + file_id)["arr_0"] for file_id in test_img], axis=0)
 x_test = x_test / 255
 
+# loading test labels
+testing_labels = f"{main_path}/labels/test/"
+test_lbl = next(os.walk(testing_labels))[2]
+test_lbl.sort()
+y_true = np.concatenate([np.load(testing_labels + file_id)["arr_0"] for file_id in test_lbl], axis=0)
+y_true = y_true.astype("float64")  # / 255
+y_true = y_true.flatten()
+
 # testing model
 model = tf.keras.models.load_model(f"{main_path}/checkpoint")
-preds = model.predict(x_test)
+y_pred = model.predict(x_test)
+y_pred = y_pred.flatten()
 
-# saving predictions
-np.savez_compressed(f"{main_path}/predictions/preds.npz", preds)
+# metrics
+dice = dice_coef(y_true, y_pred)
+auc, f1, acc, sen, spe, jac = image_metrics(y_true, y_pred, lim=0.5)
+
+print(
+    f"Accuracy\t=\t{acc}\nF1 score\t=\t{f1}\nAUC\t\t=\t{auc}\nDice\t\t=\t{dice}\nSensitivity\t=\t{sen}\nSpecificity\t=\t{spe}\nJaccard Index\t=\t{jac}"
+)
